850008f5aaece9507eff3429be4d7499750a41ee
vypham <uty.tra@thebargainvillage.com>
2024-02-06 17:29:10 +0700

    message

--- walmart_scraper/pipelinesnew.py
+++ walmart_scraper/pipelinesnew.py
@@ -30,58 +30,77 @@ class PostgresNoDuplicatesPipeline:
         # """)
 
     def process_item(self, item, spider):
-        ## Check to see if text is already in database
-        self.cur.execute(
-            """select * from seller_product_data where product_id = %s and sys_run_date= %s""",
-            (item["product_id"], item["sys_run_date"]),
-        )
-        result = self.cur.fetchone()
+        ## Check to see if the product's price is less than or equal to 55
+        try:
+            # Ensure that product_price is converted to a float for comparison
+            product_price = float(
+                item.get("product_price", 0)
+            )  # Default to 0 if not present
+        except ValueError:
+            # Log and skip items with invalid price data
+            spider.logger.warn("Invalid price for item: %s" % item["product_id"])
+            return item  # Skip items with invalid price data
 
-        ## If it is in DB, create log message
-        if result:
-            spider.logger.warn("Item already in database: %s" % item["product_id"])
-
-        ## If text isn't in the DB, insert data
-        else:
-            ## Define insert statement
+        if product_price <= 55:
+            ## Check to see if text is already in database
             self.cur.execute(
-                """
-                INSERT INTO seller_product_data 
-                (web_scraper_start_url, product_id, product_type, product_title, product_brand, product_rating,
-                product_availabilityStatus, product_price, product_original_price, product_currencyUnit, 
-                product_url_href, product_sellername, product_upc, product_sku, product_review_count,
-                product_image_1_src, product_image_2_src, product_image_3_src, product_image_4_src, 
-                product_image_5_src, product_category, product_variants, sys_run_date) 
-                VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)""",
-                (
-                    item["web_scraper_start_url"],
-                    item["product_id"],
-                    item["product_type"],
-                    item["product_title"],
-                    item["product_brand"],
-                    item["product_rating"],
-                    item["product_availabilityStatus"],
-                    item["product_price"],
-                    item["product_original_price"],
-                    item["product_currencyUnit"],
-                    item["product_url_href"],
-                    item["product_sellername"],
-                    item["product_upc"],
-                    item["product_sku"],
-                    item["product_review_count"],
-                    item["product_image_1_src"],
-                    item["product_image_2_src"],
-                    item["product_image_3_src"],
-                    item["product_image_4_src"],
-                    item["product_image_5_src"],
-                    item["product_category"],
-                    item["product_variants"],
-                    item["sys_run_date"],
-                ),
+                """SELECT * FROM seller_product_data WHERE product_id = %s AND sys_run_date = %s""",
+                (item["product_id"], item["sys_run_date"]),
+            )
+            result = self.cur.fetchone()
+
+            ## If it is in DB, create log message
+            if result:
+                spider.logger.warn("Item already in database: %s" % item["product_id"])
+
+            ## If text isn't in the DB, insert data
+            else:
+                ## Define insert statement
+                self.cur.execute(
+                    """
+                    INSERT INTO seller_product_data 
+                    (web_scraper_start_url, product_id, product_type, product_title, product_brand, product_rating,
+                    product_availabilityStatus, product_price, product_original_price, product_currencyUnit, 
+                    product_url_href, product_sellername, product_upc, product_sku, product_review_count,
+                    product_image_1_src, product_image_2_src, product_image_3_src, product_image_4_src, 
+                    product_image_5_src, product_category, product_variants, sys_run_date) 
+                    VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)""",
+                    (
+                        item["web_scraper_start_url"],
+                        item["product_id"],
+                        item["product_type"],
+                        item["product_title"],
+                        item["product_brand"],
+                        item["product_rating"],
+                        item["product_availabilityStatus"],
+                        item["product_price"],
+                        item["product_original_price"],
+                        item["product_currencyUnit"],
+                        item["product_url_href"],
+                        item["product_sellername"],
+                        item["product_upc"],
+                        item["product_sku"],
+                        item["product_review_count"],
+                        item["product_image_1_src"],
+                        item["product_image_2_src"],
+                        item["product_image_3_src"],
+                        item["product_image_4_src"],
+                        item["product_image_5_src"],
+                        item["product_category"],
+                        item["product_variants"],
+                        item["sys_run_date"],
+                    ),
+                )
+
+                ## Execute insert of data into database
+                self.connection.commit()
+        else:
+            ## If product price is greater than 55, log message and skip insertion
+            spider.logger.info(
+                "Item price exceeds limit (>$55) and will not be inserted: %s"
+                % item["product_id"]
             )
 
-            ## Execute insert of data into database
-            self.connection.commit()
         return item
 
     def close_spider(self, spider):
--- walmart_scraper/spiders/walmart.py
+++ walmart_scraper/spiders/walmart.py
@@ -4,83 +4,44 @@ import scrapy
 from urllib.parse import urlencode
 from datetime import datetime
 from walmart_scraper.items import WalmartScraperItem
+import time
+import re
 
 
 class WalmartSpider(scrapy.Spider):
     name = "walmart"
 
-    # custom_settings = {
-    #     "FEEDS": {
-    #         "data/%(name)s_%(time)s.csv": {
-    #             "format": "csv",
-    #         }
-    #     }
-    # }
+    custom_settings = {
+        "RETRY_HTTP_CODES": [429, 500, 502, 503, 504],
+        "RETRY_TIMES": 10,  # Adjust based on how aggressively you wish to retry
+        "DOWNLOAD_DELAY": 1,  # Adjust delay between requests to respect the site's rate limit
+        # Uncomment and adjust if you want to specify output format and path dynamically
+        # "FEEDS": {
+        #     "data/%(name)s_%(time)s.csv": {
+        #         "format": "csv",
+        #     }
+        # },
+    }
 
     def start_requests(self):
-        keyword_list = [
-            "Snacks",
-            "Beverages",
-            "Canned Goods",
-            "Frozen Foods",
-            "Dairy",
-            "Fresh Produce",
-            "Bakery",
-            "Pantry Staples",
-            "Apples",
-            "Milk",
-            "Bread",
-            "Eggs",
-            "Chicken",
-            "Pasta",
-            "Rice",
-            "Cereal",
-            "Kellogg",
-            "Kraft",
-            "General Mills",
-            "Coca-Cola",
-            "Nestle",
-            "Heinz",
-            "Campbell",
-            "Gluten-Free",
-            "Organic",
-            "Vegan",
-            "Low-Sodium",
-            "Sugar-Free",
-            "Bulk",
-            "Family Size",
-            "Individual Serving",
-            "Value Pack",
-            "Holiday Specials",
-            "Party Supplies",
-            "BBQ Essentials",
-            "Organic",
-            "Low-Fat",
-            "High-Fiber",
-            "Non-GMO",
-            "Keto",
-            "Paleo",
-            "Atkins",
-            "grocery",
+        # List of Walmart search URLs
+        search_urls = [
+            # "https://www.walmart.com/shop/deals?",
+            "https://www.walmart.com/shop/deals/household-essentials?povid=HHE_HHECP_hubspoke_shopbyprice_hhedeals&",
+            # Add more URLs as needed
         ]
-        # for keyword in keyword_list:
-        payload = {
-            # "q": keyword,
-            # "sort": "price_low",
-            "page": 1,
-            "affinityOverride": "default",
-        }
-        walmart_search_url = "https://www.walmart.com/shop/deals?" + urlencode(
-            payload
-        )  # "https://www.walmart.com/search?" + urlencode(payload)
-        yield scrapy.Request(
-            url=walmart_search_url,
-            callback=self.parse_search_results,
-            meta={
-                # "keyword": keyword,
-                "page": 1
-            },
-        )
+
+        for walmart_search_url in search_urls:
+            payload = {
+                "page": 1,
+                "affinityOverride": "default",
+            }
+
+            yield scrapy.Request(
+                url=walmart_search_url + urlencode(payload),
+                callback=self.parse_search_results,
+                meta={"page": 1},
+            )
 
     def parse_search_results(self, response):
         page = response.meta["page"]
@@ -165,18 +126,26 @@ class WalmartSpider(scrapy.Spider):
 
     def parse_product_detail(self, response):
         # Function to check if product_id is in the list of products for a variant
-        def find_variant(variant_criteria, variant_id, desired_product_id):
-            if variant_criteria is None:
+        def find_variant_by_pattern(all_variant_criteria, pattern, desired_product_id):
+            if all_variant_criteria is None:
                 return None
 
-            return next(
-                (
-                    variant["name"]
-                    for variant in variant_criteria.get("variantList", [])
-                    if desired_product_id in variant.get("products", [])
-                ),
-                None,
-            )
+            # Compile the regex pattern for case-insensitive search
+            pattern_regex = re.compile(pattern, re.IGNORECASE)
+
+            # Search for criteria that matches the pattern
+            for variant_criteria in all_variant_criteria:
+                if pattern_regex.search(variant_criteria["id"]):
+                    # Once the matching criteria is found, search its variantList
+                    return next(
+                        (
+                            variant["name"]
+                            for variant in variant_criteria.get("variantList", [])
+                            if desired_product_id in variant.get("products", [])
+                        ),
+                        None,
+                    )
+            return None
 
         script_tag = response.xpath('//script[@id="__NEXT_DATA__"]/text()').get()
         if script_tag is not None:
@@ -185,121 +154,135 @@ class WalmartSpider(scrapy.Spider):
                 "product"
             ]
             desired_product_id = raw_product_data.get("id")
-            # Extract color variants
-            color_variants = find_variant(
-                next(
-                    (
-                        variant_criteria
-                        for variant_criteria in raw_product_data.get(
-                            "variantCriteria", []
-                        )
-                        if variant_criteria["id"] == "actual_color"
-                    ),
-                    None,
-                ),
-                "actual_color",
+
+            # Extract color and size variants using the updated function
+            color_variants = find_variant_by_pattern(
+                raw_product_data.get("variantCriteria", []),
+                ".*color.*",
                 desired_product_id,
             )
-
-            # Extract clothing size variants
-            clothing_size_variants = find_variant(
-                next(
-                    (
-                        variant_criteria
-                        for variant_criteria in raw_product_data.get(
-                            "variantCriteria", []
-                        )
-                        if variant_criteria["id"] == "clothing_size"
-                    ),
-                    None,
-                ),
-                "clothing_size",
+            size_variants = find_variant_by_pattern(
+                raw_product_data.get("variantCriteria", []),
+                ".*size.*",
                 desired_product_id,
             )
 
             yield {
                 "web_scraper_start_url": "walmart",
-                "product_id": raw_product_data.get("id")
-                if raw_product_data.get("id")
-                else None,
-                "product_type": raw_product_data.get("type")
-                if raw_product_data.get("type")
-                else None,
-                "product_title": raw_product_data.get("name")
-                if raw_product_data.get("name")
-                else None,
-                "product_brand": raw_product_data.get("brand")
-                if raw_product_data.get("brand")
-                else None,
-                "product_rating": raw_product_data.get("averageRating")
-                if raw_product_data.get("averageRating")
-                else None,
-                "product_availabilityStatus": raw_product_data.get("availabilityStatus")
-                if raw_product_data.get("availabilityStatus")
-                else None,
-                "product_price": raw_product_data["priceInfo"]["currentPrice"].get(
-                    "price"
-                )
-                if raw_product_data["priceInfo"]["currentPrice"].get("price")
-                else None,
-                "product_original_price": raw_product_data["priceInfo"]["wasPrice"].get(
-                    "price"
-                )
-                if raw_product_data["priceInfo"]["wasPrice"]
-                else None,
-                "product_currencyUnit": raw_product_data["priceInfo"][
-                    "currentPrice"
-                ].get("currencyUnit")
-                if raw_product_data["priceInfo"]["currentPrice"]
-                else None,
-                "product_url_href": response.meta["product_url"]
-                if response.meta["product_url"]
-                else None,  # Include product URL in the output
-                "product_sellername": raw_product_data.get("sellerName")
-                if raw_product_data.get("sellerName")
-                else None,
-                "product_upc": raw_product_data.get("upc")
-                if raw_product_data.get("upc")
-                else None,
-                "product_sku": raw_product_data.get("itemId")
-                if raw_product_data.get("itemId")
-                else None,
-                "product_review_count": raw_product_data.get("numberOfReviews")
-                if raw_product_data.get("numberOfReviews")
-                else None,
-                "product_image_1_src": raw_product_data["imageInfo"].get("thumbnailUrl")
-                if raw_product_data["imageInfo"].get("thumbnailUrl")
-                else None,
-                "product_image_2_src": raw_product_data["imageInfo"]["allImages"][1][
-                    "url"
-                ]
-                if len(raw_product_data["imageInfo"]["allImages"]) >= 2
-                else None,
-                "product_image_3_src": raw_product_data["imageInfo"]["allImages"][2][
-                    "url"
-                ]
-                if len(raw_product_data["imageInfo"]["allImages"]) >= 3
-                else None,
-                "product_image_4_src": raw_product_data["imageInfo"]["allImages"][3][
-                    "url"
-                ]
-                if len(raw_product_data["imageInfo"]["allImages"]) >= 4
-                else None,
-                "product_image_5_src": raw_product_data["imageInfo"]["allImages"][4][
-                    "url"
-                ]
-                if len(raw_product_data["imageInfo"]["allImages"]) >= 5
-                else None,
-                "product_category": raw_product_data["category"]["path"][1]["name"]
-                if raw_product_data["category"]["path"][1]["name"]
-                else None,
+                "product_id": (
+                    raw_product_data.get("id") if raw_product_data.get("id") else None
+                ),
+                "product_type": (
+                    raw_product_data.get("type")
+                    if raw_product_data.get("type")
+                    else None
+                ),
+                "product_title": (
+                    raw_product_data.get("name")
+                    if raw_product_data.get("name")
+                    else None
+                ),
+                "product_brand": (
+                    raw_product_data.get("brand")
+                    if raw_product_data.get("brand")
+                    else None
+                ),
+                "product_rating": (
+                    raw_product_data.get("averageRating")
+                    if raw_product_data.get("averageRating")
+                    else None
+                ),
+                "product_availabilityStatus": (
+                    raw_product_data.get("availabilityStatus")
+                    if raw_product_data.get("availabilityStatus")
+                    else None
+                ),
+                "product_price": (
+                    raw_product_data["priceInfo"]["currentPrice"].get("price")
+                    if raw_product_data["priceInfo"]["currentPrice"].get("price")
+                    else None
+                ),
+                "product_original_price": (
+                    raw_product_data["priceInfo"]["wasPrice"].get("price")
+                    if raw_product_data["priceInfo"]["wasPrice"]
+                    else None
+                ),
+                "product_currencyUnit": (
+                    raw_product_data["priceInfo"]["currentPrice"].get("currencyUnit")
+                    if raw_product_data["priceInfo"]["currentPrice"]
+                    else None
+                ),
+                "product_url_href": (
+                    response.meta["product_url"]
+                    if response.meta["product_url"]
+                    else None
+                ),  # Include product URL in the output
+                "product_sellername": (
+                    raw_product_data.get("sellerName")
+                    if raw_product_data.get("sellerName")
+                    else None
+                ),
+                "product_upc": (
+                    raw_product_data.get("upc") if raw_product_data.get("upc") else None
+                ),
+                "product_sku": (
+                    raw_product_data.get("itemId")
+                    if raw_product_data.get("itemId")
+                    else None
+                ),
+                "product_review_count": (
+                    raw_product_data.get("numberOfReviews")
+                    if raw_product_data.get("numberOfReviews")
+                    else None
+                ),
+                "product_image_1_src": (
+                    raw_product_data["imageInfo"].get("thumbnailUrl")
+                    if raw_product_data["imageInfo"].get("thumbnailUrl")
+                    else None
+                ),
+                "product_image_2_src": (
+                    raw_product_data["imageInfo"]["allImages"][1]["url"]
+                    if len(raw_product_data["imageInfo"]["allImages"]) >= 2
+                    else None
+                ),
+                "product_image_3_src": (
+                    raw_product_data["imageInfo"]["allImages"][2]["url"]
+                    if len(raw_product_data["imageInfo"]["allImages"]) >= 3
+                    else None
+                ),
+                "product_image_4_src": (
+                    raw_product_data["imageInfo"]["allImages"][3]["url"]
+                    if len(raw_product_data["imageInfo"]["allImages"]) >= 4
+                    else None
+                ),
+                "product_image_5_src": (
+                    raw_product_data["imageInfo"]["allImages"][4]["url"]
+                    if len(raw_product_data["imageInfo"]["allImages"]) >= 5
+                    else None
+                ),
+                "product_category": (
+                    raw_product_data["category"]["path"][1]["name"]
+                    if raw_product_data["category"]["path"][1]["name"]
+                    else None
+                ),
                 "product_variants": json.dumps(
                     {
                         "color": color_variants,
-                        "size": clothing_size_variants,
+                        "size": size_variants,
                     }
                 ),
                 "sys_run_date": datetime.now().strftime(
                     "%Y-%m-%d"
                 ),  # Include current date
             }
+
+    def retry_request(self, response):
+        """Retry request with exponential backoff."""
+        retry_times = response.meta.get("retry_times", 0) + 1
+        if retry_times <= self.custom_settings.get("RETRY_TIMES", 10):
+            wait_time = math.pow(2, retry_times)  # Exponential backoff
+            self.logger.info(f"Waiting {wait_time} seconds to retry...")
+            time.sleep(wait_time)
+            return response.request.replace(
+                meta={"retry_times": retry_times}, dont_filter=True
+            )
