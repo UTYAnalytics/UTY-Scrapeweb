2a8826f35a0700d81f29e8a166860f668475fcea
vypham <uty.tra@thebargainvillage.com>
2024-02-07 15:47:41 +0700

    message

--- walmart_scraper/spiders/walmart.py
+++ walmart_scraper/spiders/walmart.py
@@ -26,10 +26,10 @@ class WalmartSpider(scrapy.Spider):
     def start_requests(self):
         # List of Walmart search URLs
         search_urls = [
-            "https://www.walmart.com/shop/deals?",
-            "https://www.walmart.com/shop/deals/household-essentials?povid=HHE_HHECP_hubspoke_shopbyprice_hhedeals&",
-            "https://www.walmart.com/browse/976759?athcpid=7cbe8a0a-c5fe-4839-a851-edb3ec71638c&athpgid=AthenaContentPage&athznid=athenaModuleZone&athmtid=AthenaItemCarousel&athtvid=4&athena=true&",  # Food
-            "https://www.walmart.com/browse/personal-care/1005862_1071969?povid=EDN_EDNCP_Feb_itemcarousel_carousel_PC&",  # All Bath & Body
+            "https://www.walmart.com/shop/deals?facet=retailer_type%3AWalmart&",
+            "https://www.walmart.com/shop/deals/stock-up-essentials?povid=EDN_EDNCP_x_LHN_Savings_GroceriesEssentials&facet=retailer_type%3AWalmart&",
+            "https://www.walmart.com/browse/personal-care&facet=retailer_type%3AWalmart&",
+            "https://www.walmart.com/shop/household-essentials-stock-up?povid=EDN_EDNCP_Feb_itemcarousel_carousel_HHE&facet=retailer_type%3AWalmart&",
             # Add more URLs as needed
         ]
 
